57cb8b02d94cf218fe2d8aefad774e49439dde35
Kim Larsson <kim@humbleguys.se>
2022-09-09 16:38:59 +0200

    Preperad for version 0.0.1

--- src/resources/components/base.blade.php
+++ src/resources/components/base.blade.php
@@ -10,3 +10,10 @@
 >
     {!! $slot !!}
 </div>
+
+@once
+    @push('head')
+        <link rel="stylesheet" href="{{ asset('../vendor/humble-guys/humble-photoswipe-component/public/resources/dist/assets/index.1201b41e.css') }}">
+        <script module defer src="{{ asset('../vendor/humble-guys/humble-photoswipe-component/public/resources/dist/assets/index.262348ba.js') }}"></script>
+    @endpush   
+@endonce 
